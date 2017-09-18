880a986cf7f1a0254bd3ad6501588afcb23027c3
Joseph Constantakis <jcon5294@gmail.com>
2017-09-18 12:59:07 -0700

    first commit - passable version of first page with bg video, mute button, banner text, and app store link

---
+++ assets/js/bgvideo.js
@@ -0,0 +1,35 @@
+var player;
+function onYouTubeIframeAPIReady() {
+    var videoId = 'kjkjmTm0XD4';
+    player = new YT.Player('video-background', {
+        videoId: videoId, // YouTube Video ID
+        playerVars: {
+            autoplay: 1, // Auto-play the video on load
+            controls: 0, // Show pause/play buttons in player
+            showinfo: 0, // Hide the video title
+            modestbranding: 1, // Hide the Youtube Logo
+            loop: 1, // Run the video in a loop
+            fs: 0, // Hide the full screen button
+            cc_load_policy: 0, // Hide closed captions
+            iv_load_policy: 3, // Hide the Video Annotations
+            autohide: 0, // Hide video controls when playing
+            rel: 0,
+            playlist: videoId
+        }
+    });
+}
+
+// Mute button
+$('#mute').on('click', function() {
+	var button = $('#mute');
+	var soundOn = button.hasClass('sound-on');
+	if (soundOn) {
+		button.removeClass('sound-on');
+		button.addClass('sound-off');
+		player.mute();
+	} else {
+		button.removeClass('sound-off');
+		button.addClass('sound-on');
+		player.unMute();
+	}
+});
