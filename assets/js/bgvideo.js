c7283b0ae439eee96fc93ff0f44f049808119c7d
Joseph Constantakis <jcon5294@gmail.com>
2017-09-28 20:17:08 -0700

    remove youtube nonsense

--- assets/js/bgvideo.js
+++ assets/js/bgvideo.js
@@ -1,37 +1,8 @@
-var player = {};
-function onYouTubeIframeAPIReady() {
-  if (isMobile.any()) {
-    return;
-  }
-  var videoId = 'kjkjmTm0XD4';
-  player = new YT.Player('yt-embed', {
-    videoId: videoId, // YouTube Video ID
-    playerVars: {
-      autoplay: 1, // Auto-play the video on load
-      controls: 0, // Show pause/play buttons in player
-      showinfo: 0, // Hide the video title
-      modestbranding: 1, // Hide the Youtube Logo
-      loop: 1, // Run the video in a loop
-      fs: 0, // Hide the full screen button
-      cc_load_policy: 0, // Hide closed captions
-      iv_load_policy: 3, // Hide the Video Annotations
-      autohide: 0, // Hide video controls when playing
-      rel: 0,
-      playlist: videoId
-    }
-  });
-}
-
 // Mute button
 function toggleMute() {
   var button = $('#mute');
   var soundOn = button.hasClass('sound-on');
   $("#html5-embed").prop('muted', soundOn);
-  if (soundOn) {
-    player.mute && player.mute();
-  } else {
-    player.unMute && player.unMute();
-  }
   redrawMuteButton(soundOn);
 }
 
@@ -51,8 +22,6 @@ function loaded() {
   var smallSafari = !!navigator.userAgent.match(/Version\/[\d\.]+.*Safari/) && !isMobile.iPad();
 
   if (mobile) {
-    $('#yt-frame-api').remove();
-    $('#yt-embed').remove();
     $('#banner, .content').addClass('mobile');
     $('.video-background').addClass('mobile');
     if (smallSafari) {
@@ -60,7 +29,7 @@ function loaded() {
       $('.video-background').addClass('mobilesafari');
     }
   } else {
-    $('#html5-embed').remove();
+    $('#html5-embed').prop('muted', false);
   }
   redrawMuteButton(mobile);
 }
