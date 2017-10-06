81aff24f81e74292bde50f3c3a463bcd52b60209
Joseph Constantakis <jcon5294@gmail.com>
2017-10-06 02:20:53 -0700

    updates for in-app browsing experience - layout, move some paragraphs around. also be more fair to line 6 adapter bc the irig went up in price again

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,33 @@
 /*
-	Landed by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+  Landed by HTML5 UP
+  html5up.net | @ajlkn
+  Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 (function($) {
 
-	$(function() {
+  $(function() {
 
-		// Fix: Placeholder polyfill.
-			$('form').placeholder();
+    // Fix: Placeholder polyfill.
+      $('form').placeholder();
 
-		// Scrolly links.
-			$('.scrolly').scrolly({
-				speed: 500
-			});
-	});
+    // Scrolly links.
+      $('.scrolly').scrolly({
+        speed: 500
+      });
+
+      if (getParameterByName("inapp")) {
+        $('html').addClass('inapp');
+      }
+  });
 
 })(jQuery);
+
+function getParameterByName(name) {
+    var url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
